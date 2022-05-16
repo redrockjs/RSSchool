7745ec927da0d3f7e5aaec205885a07ba8faf4f6
redrockjs <smb.sochi@gmail.com>
2022-05-16 16:25:03 +0300

    fix: finish 03-files-in-folder task

--- libfoo/03-files-in-folder/index.js
+++ libfoo/03-files-in-folder/index.js
@@ -3,18 +3,17 @@ const path = require('path');
 const st = require('fs');
 
 fs.readdir(path.join(__dirname, 'secret-folder'), {withFileTypes: true})
-  .then( files => {
+  .then(files => {
     files.forEach(file => {
-      if (file.isFile() === true) {
+      file.isFile() && (
         st.stat(path.join(__dirname, 'secret-folder', file.name), (err, stats) => {
-          let currentFile = path.parse(file.name);
+          const currentFile = path.parse(file.name);
           console.log(currentFile.name + ' - ' + currentFile.ext.substring(1) + ' - ' + stats.size + ' bytes');
-        });
-      }
+        }));
     });
   })
-  .catch( err => {
-    if (err !== undefined) console.log(err);
+  .catch(err => {
+    (err !== undefined) && console.log(err);
   });
 
 // <имя файла>-<расширение файла>-<вес файла>
