3bff9ab2cdf34babe29a5f8665a147c9ca013b39
redrockjs <smb.sochi@gmail.com>
2022-05-16 15:29:45 +0300

    fix: finish 01-read-file task

---
+++ libfoo/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const fs = require('fs');
+
+const fileName = path.join(__dirname, 'text.txt');
+const stream = new fs.ReadStream(fileName, {encoding: 'utf-8'});
+
+stream.on('readable', function () {
+  const data = stream.read();
+  if (data != null) console.log(data);
+});
+
+stream.on('error', function (err) {
+  console.log(err.message);
+});
+
