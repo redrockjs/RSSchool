c2978ef2c478a3295b43cbe5f05281d1736e30cb
redrockjs <smb.sochi@gmail.com>
2022-05-17 09:07:26 +0300

    fix: finish 04-copy-directory

--- libfoo/04-copy-directory/index.js
+++ libfoo/04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require('fs/promises');
+const path = require('path');
+const sourceDir = 'files';
+const targetDir = 'files-copy';
 
+const readDirectory = () => {
+  fs.readdir(path.join(__dirname, sourceDir), {withFileTypes: true})
+    .then(files => {
+      files.forEach(file => {
+        fs.copyFile(path.join(__dirname, sourceDir, file.name), path.join(__dirname, targetDir, file.name), 0)
+          .then(() => console.log(`${file.name} - ok`))
+          .catch(err => console.log('Copy failed by reason ', err.code));
+      });
+    })
+    .catch(err => (err !== undefined) && console.log(err));
+};
+
+fs.rm(path.join(__dirname, targetDir), {force: true, recursive: true})
+  .then(() => {
+    fs.mkdir(path.join(__dirname, targetDir))
+      .then(() => readDirectory())
+      .catch(err => console.log('Make folder failed by reason', err.code));
+  });
